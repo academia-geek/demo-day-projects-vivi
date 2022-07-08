43f38de61e631ea7f982861789e8e92e45ea15dc
Sebastian Luna <sebas1997321@gmail.com>
2022-07-08 18:39:39 -0500

    initial structure

---
+++ vivi/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { AppRoutes } from './routes/AppRoutes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>
+);
+

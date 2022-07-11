1c653ced33b4099de7cfd5e478d22cb4073107bd
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-11 10:20:05 -0500

    Modificaciones del Banner

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,24 +1,34 @@
-import { Alert, Button, Space } from 'antd';
+import { Alert, Button, Space, Typography } from 'antd';
 import 'antd/dist/antd.css'
-
+import carro from '../assets/carro3.png'
+const { Title,Text } = Typography;
 
 export const AlertVivi = () => (
   <>
    
     <Alert
-    //   message="Conoce la mejor forma de promover el turismo de tu región,unete a la familia ViVi "
-      type="warning"
-      action={
-        <div>
-            <h1>ViVi Empresarial</h1>
-            <h4> Conoce la mejor forma de promover el turismo de tu región,unete a la familia ViVi </h4>
-        <Space>
-         <Button size="small" type="ghost">
-          Conoce mas 
-          </Button>
-        </Space>
-        </div>
-      }
+   
+    message = {
+    <div style={{display:"flex"}}>
+      
+      <div style={{textAlign:"right",marginLeft:"300px"}}>
+    <Title style={{color:"white"}}>VIVI EMPRESARIAL</Title>
+    <Text type="secondary">
+      Conoce la mejor forma de promover el turismo de tu región,unete a la familia ViVi. 
+    </Text>
+    </div>
+    <img src={carro} style={{width:"10vw"}}/>
+    <Button type="ghost" style={{marginLeft:"2vw",marginTop:"40px",color:"white"}}>CONOCE MAS</Button>
+    </div>
+    } 
+    description=
+    { <div>
+   
+    
+    </div>
+  }
+    style={{backgroundColor:"rgba(132,134,151)",height:"109px"}}
+   
       closable
     />
    

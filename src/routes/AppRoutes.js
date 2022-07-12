39056190a7cc5799310d6a1a7539392170150c8a
7-18 <kevinbrianbr@gmail.com>
2022-07-12 14:48:30 -0500

    auth, public routes and private routes"

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { LandingPage } from '../containers/LandingPage'
-import { Login } from '../components/Login'
-import { Register } from '../components/Register'
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LandingPage } from "../containers/LandingPage";
+import { Login } from "../components/Login";
+import { Register } from "../components/Register";
+import { auth } from "../firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AppRoutes = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        setIsLogged(true);
+        user.getIdToken().then((token) => {
+          return token;
+        });
+      } else {
+        setIsLogged(false);
+      }
+    });
+  }, []);
   return (
     <BrowserRouter>
-
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Register />} />
       </Routes>
     </BrowserRouter>
-
-  )
-}
+  );
+};

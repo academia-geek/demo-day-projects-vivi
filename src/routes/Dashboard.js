e5416f88dc239148d242648a478985a3e2a0c1c4
7-18 <kevinbrianbr@gmail.com>
2022-07-12 14:54:59 -0500

    auth, public routes and private routes"

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import { Home } from "../containers/Home";
 
 export const Dashboard = () => {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<div>Home</div>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );

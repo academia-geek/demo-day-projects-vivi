16b9abc0dd35b760903eae50dfad0eeea3423a03
7-18 <kevinbrianbr@gmail.com>
2022-07-12 19:00:48 -0500

    spinner

--- src/components/Spin.jsx
+++ src/components/Spin.jsx
@@ -1,10 +1,10 @@
 import { Spinner } from "react-bootstrap";
 
-export const Spin = ({ heightPx }) => {
+export const Spin = ({ height }) => {
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ height: heightPx }}
+      style={{ height: height }}
     >
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>

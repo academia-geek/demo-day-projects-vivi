9991ad997905e4009f600bf18f8b114b7a0a0a98
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-14 07:58:46 -0500

    add Sider

---
+++ src/components/Sider.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+  } from '@ant-design/icons';
+const { Sider } = Layout;
+const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }));
+export const SiderCalendar = () => {
+  return (
+    <Layout hasSider>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '90vh',
+        position: 'fixed',
+        left: '83.1vw',
+        top:'4.8vw',
+        bottom: 0,
+      }}
+    >
+      <div className="logo" />
+      
+      <Menu  mode="inline" defaultSelectedKeys={['10']} />
+      {/* <h1>Anuncios</h1> */}
+      <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+    </Sider>
+    </Layout>
+  )
+}
--- src/containers/CalendarPage.jsx
+++ src/containers/CalendarPage.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import  { MainCalendar } from '../components/Calendar'
-
+import {  Col } from "react-bootstrap";
 import { NavbarHome } from '../components/NavbarHome'
+import { SiderCalendar } from '../components/Sider';
 
 export const CalendarPage = () => {
   return (
     <div>
         <NavbarHome/>
+        <div className="d-flex">
+        <Col sm={9}>
         <MainCalendar/>
+        <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br /> 
+        </Col>
+        <Col sm={3}>
+          {/* <Aside />
+          <ListAnun/> */}
+          <SiderCalendar/>
+        </Col>
+        </div>
     </div>
   )
 }

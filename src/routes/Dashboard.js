54ea0f323e0bfac16e3ca35447885bfb1c406fc9
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-14 13:01:46 -0500

    Schedule Form

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { AddEvent } from "../containers/AddEvent";
 import { CalendarPage } from "../containers/CalendarPage";
+import { EventSchedule } from "../containers/EventSchedule";
 import { Home } from "../containers/Home";
 
 export const Dashboard = () => {
@@ -10,6 +11,7 @@ export const Dashboard = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/addEvent" element={<AddEvent/>} />
+        <Route path="/Schedule" element={<EventSchedule/>} />
       </Routes>
     </>
   );

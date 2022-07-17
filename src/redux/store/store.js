1ebaca96e12de1787842801d03703d694d6e83a0
Sebastian Luna <sebas1997321@gmail.com>
2022-07-17 06:05:13 -0500

    sunday merge

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,28 +3,17 @@ import thunk from "redux-thunk";
 import { loginReducer } from "../reducers/loginReducer";
 import { registerReducer } from "../reducers/registerReducer";
 import { eventsReducers } from "../reducers/eventsReducer";
-<<<<<<< HEAD
 import { infoReducers } from "../reducers/infoReducer";
-=======
 import { scheduleReducers } from "../reducers/schedule";
->>>>>>> 797d3cf97c9be53b4ea1927f35ea7037eb01153e
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
-  deseados: deseadosReducers,
   eventos:eventsReducers,
   register: registerReducer,
   login: loginReducer,
-<<<<<<< HEAD
-  info: infoReducers
-=======
-  gustos: gustosReducers,
-  visitados: visitadosReducers,
-  posts: postsReducers,
-  schedule:scheduleReducers
-  
->>>>>>> 797d3cf97c9be53b4ea1927f35ea7037eb01153e
+  info: infoReducers,
+  schedule: scheduleReducers
 });
 
 export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -7,31 +7,8 @@ export const typesLogin = {
   LOGOUT: "LOGOUT",
 };
 
-<<<<<<< HEAD
 export const typesInfo = {
   list_info: 'list',
-=======
-export const typesGustos = {
-  add_gustos: '[likes]agregar',
-  list_gustos: '[likes]list',
-  delete_gustos: '[likes] liminar',
-}
-
-export const typesVisitados = {
-  add_visit: '[Visit]agregar',
-  list_visit: '[Visit]list',
-  delete_visit: '[Visit]eliminar',
-}
-export const typesDeseados = {
-  add_deseado: '[wish]agregar',
-  list_deseado: '[wish]list',
-  delete_deseado: '[wish]eliminar',
-}
-export const typesPosts = {
-  add_post: '[Posts]agregar',
-  list_post: '[Posts]list',
-  delete_post: '[Posts]eliminar',
->>>>>>> 797d3cf97c9be53b4ea1927f35ea7037eb01153e
 }
 export const typesEvents = {
   add: '[Events]agregar',

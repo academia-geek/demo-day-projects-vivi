c8aa8e28139bfa17e23a3df39274a411523d65a6
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-18 21:35:11 -0500

    default message change

--- src/components/calendar _and_programming/CardEvent.jsx
+++ src/components/calendar _and_programming/CardEvent.jsx
@@ -6,28 +6,28 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { useDispatch,useSelector} from 'react-redux';
 import image from '../../assets/prueba/image3.png'
+export const CardEvent = ({ m }) => {
+  const dispatch = useDispatch()
+  const h = m + 86400000
+  const { EventsList } = useSelector(store => store.eventos)
+  const filtro = EventsList.filter((ev) => {
+    return ev.date.map((date) => {
+      return date.seconds;
+    }).includes((h / 1000));
+  });
+  console.log(filtro)
+  useEffect(() => {
+    dispatch(listEventAsync())
+  }, [dispatch])
 
-
-export const CardEvent = ({m}) => {
-    const dispatch=useDispatch()
-         const h =m+86400000
-           const {EventsList} = useSelector(store => store.eventos)
-              const filtro = EventsList.filter((ev) => {
-        return ev.date.map((date) => { 
-          return date.seconds;
-        }).includes((h/1000));
-      });
-    console.log(filtro)
-      useEffect(()=>{
-        dispatch(listEventAsync())
-    },[dispatch])
-  
   return (
     <div>
     <List
     style={{marginTop:"80px",marginLeft:"80px",border:'1px solid rgba(255, 189, 41, 1)'}}
     itemLayout="horizontal"
     dataSource={filtro}
+    locale={{emptyText:'No hay eventos programados para este día'}}
+    emptyText='no'
     renderItem={(item) => (
       <List.Item>
       <div style={{display:"flex"}}>

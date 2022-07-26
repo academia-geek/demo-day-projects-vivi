2648063454ce33413d0149c809563fbb6b039841
SebastianLunar <sebas1997321@gmail.com>
2022-07-26 01:40:00 -0500

    ordering by time

--- src/helpers/timeConverter.js
+++ src/helpers/timeConverter.js
@@ -1,14 +1,10 @@
 
 const timeConverter = (tiempo) => {
     const pad = v => v.padStart(2, `0`);
-    const initialDate = tiempo
-        .split(/[-/]/).map(pad).join("/");
-    const toFragments = dateString => initialDate
-        .split(/[-/]/).map(pad);
-    const dateTo_mmddyyyy = ([date, month, year], divider = "/") =>
-        `${month}${divider}${date}${divider}${year}`;
+    const initialDate = tiempo.split(/[-/]/).map(pad).join("/");
+    const toFragments = dateString => initialDate.split(/[-/]/).map(pad);
     const [date, month, year] = toFragments(initialDate);
-    console.log(`initial (dd/mm/yyyy): ${initialDate}`);
-    console.log(`reformatted to mm/dd/yyyy (array join): ${[month, date, year].join('/')}`);
-    console.log(`reformatted to mm-dd-yyyy (function): ${dateTo_mmddyyyy(toFragments(initialDate), "-")}`);
+    const nuevo = [month, date, year].join('/');
+
+    return nuevo
 }

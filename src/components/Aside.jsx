36b00a2a2a55115c45de2486ce3f6733748dee38
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-25 23:01:56 -0500

    data event

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -12,29 +12,43 @@ import "react-calendar/dist/Calendar.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux'
 import moment from "moment";
-import { listAsync } from "../redux/actions/infoAction";
+import {  listAsync } from "../redux/actions/infoAction";
 import { listScheduleAsync } from "../redux/actions/scheduleAction";
 import { AsideEvent } from "./calendar _and_programming/AsideEvent";
-const datadate = []
+import { elementAcceptingRef } from "@mui/utils";
+import { listEventAsync } from "../redux/actions/eventsAction";
+const datadate = [{name:""}]
 export const Aside = () => {
   const [value] = useState(new Date());
   const navigate = useNavigate();
-   const { Activities } = useSelector(store => store.schedule)
+  //  const { Activities } = useSelector(store => store.schedule)
+  const { EventsList } = useSelector(store => store.eventos)
   const dispatch = useDispatch()
-  console.log(Activities)
-  const dateToday = new Date(moment().format("YYYY-MM-DD")).getTime();
+   const dateToday = new Date(moment().format("YYYY-MM-DD")).getTime();
   console.log(dateToday)
   useEffect(() => {
-    dispatch(listScheduleAsync());
-
+    dispatch(listEventAsync());
   }, [dispatch]);
+  console.log(EventsList)
   for (let i = 0; i <= 14; i++) {
     const fecha = dateToday + (86400000 * i)
     console.log(fecha)
-    const filtro = Activities?.find(element => element.dates == fecha)
-    if (filtro != undefined) {
-      const count = datadate.push(filtro)
+    const filtro = EventsList.find((ev) => {
+      return ev.date
+        .map((date) => {
+          return date.seconds;
+        })
+        .includes(fecha/1000);
+    });
+    console.log(filtro)
+    const prueba=datadate.some(elem =>elem?.name === filtro?.name)
+    if(prueba != true ){
+      datadate.push(filtro)
     }
+    // const filtro = Activities?.find(element => element.dates == fecha)
+    // if (filtro != undefined) {
+    //   const count = datadate.push(filtro)
+    // }
   }
   console.log(datadate)
   return (
@@ -51,10 +65,10 @@ export const Aside = () => {
           datadate?.map(item =>
             <CardStyled>
               <div>
-              <AsideEvent id={item.id}/>
+              {/* <AsideEvent id={item?.id}/> */}
               <div style={{display:"flex",marginTop:"1vw",marginLeft:"2vw"}}>
-              <h6 style={{fontSize:"12px",fontWeight: "400",width:"30%"}}>{item.date}</h6>
-              <h6 style={{fontSize:"14px",fontWeight: "500",width:"60%",marginLeft:"4px"}}>{item.name}</h6>
+              {/* <h6 style={{fontSize:"12px",fontWeight: "400",width:"30%"}}>{item.date}</h6> */}
+              <h6 style={{fontSize:"14px",fontWeight: "500",width:"60%",marginLeft:"4px"}}>{item?.name}</h6>
               </div>
               </div>
             </CardStyled>

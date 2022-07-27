abef0839535963ad1f30264822c24733cc7ea601
SebastianLunar <sebas1997321@gmail.com>
2022-07-27 03:45:37 -0500

    "otros_usuarios"

--- src/components/OtherUsers.jsx
+++ src/components/OtherUsers.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { auth } from "../firebase/firebaseConfig";
 import { CONT, EditIcon, TAG, UserData, UserImg } from "../styles/globalStyles";
-import Modal from 'react-bootstrap/Modal';
 import useForm from '../hooks/useForm';
 import { addAge, addLike, addLiked, addPlace, deleteLike, deletePlace, deleteLiked, listAsync, updatePhoto } from "../redux/actions/infoAction";
 import { imgUpload } from "../helpers/imgUpload";

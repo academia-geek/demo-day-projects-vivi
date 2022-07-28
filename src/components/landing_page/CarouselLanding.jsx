a8481babf9f3933264446b8557791beff78cc228
Melisa Mendoza <melisamendozamogollon@gmail.com>
2022-07-28 01:17:59 -0500

    Detail

--- src/components/landing_page/CarouselLanding.jsx
+++ src/components/landing_page/CarouselLanding.jsx
@@ -18,138 +18,138 @@ export const CarouselLanding = () => {
     setIndex(selectedIndex);
   };
   return (
-    <div></div>
-    // <Carousel>
-    //   activeIndex={index}
-    //   onSelect={handleSelect}
-    //   as={CarouselVideos}
-    //   fade
-    // >
-    //   <Carousel.Item>
-    //     <video
-    //       autoPlay
-    //       loop
-    //       muted
-    //       className="d-block w-100"
-    //       loading="lazy"
-    //       preload="metadata"
-    //     >
-    //       <source src={colombia_1s} type="video/mp4" />
-    //     </video>
-    //     <Carousel.Caption>
-    //       <h3>Encuentra tu evento ideal</h3>
-    //       <p>
-    //         Descubre los festivales, carnavales, ferias y fiestas de Colombia
-    //         que invitan a la aventura de conocer este sorprendente país
-    //       </p>
-    //       <ButtonLanding onClick={() => navigate("/signin")}>
-    //         <span>
-    //           ¡Únete a ViVi!
-    //           <ArrowForwardIcon />
-    //         </span>
-    //       </ButtonLanding>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <video
-    //       autoPlay
-    //       loop
-    //       muted
-    //       className="d-block w-100"
-    //       loading="lazy"
-    //       preload="metadata"
-    //     >
-    //       <source src={colombia_2s} type="video/mp4" />
-    //     </video>
-    //     <Carousel.Caption>
-    //       <h3>Encuentra tu evento ideal</h3>
-    //       <p>
-    //         Descubre los festivales, carnavales, ferias y fiestas de Colombia
-    //         que invitan a la aventura de conocer este sorprendente país
-    //       </p>
-    //       <ButtonLanding onClick={() => navigate("/signin")}>
-    //         <span>
-    //           ¡Únete a ViVi!
-    //           <ArrowForwardIcon />
-    //         </span>
-    //       </ButtonLanding>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <video
-    //       autoPlay
-    //       loop
-    //       muted
-    //       className="d-block w-100"
-    //       loading="lazy"
-    //       preload="metadata"
-    //     >
-    //       <source src={colombia_3s} type="video/mp4" />
-    //     </video>
-    //     <Carousel.Caption>
-    //       <h3>Encuentra tu evento ideal</h3>
-    //       <p>
-    //         Descubre los festivales, carnavales, ferias y fiestas de Colombia
-    //         que invitan a la aventura de conocer este sorprendente país
-    //       </p>
-    //       <ButtonLanding onClick={() => navigate("/signin")}>
-    //         <span>
-    //           ¡Únete a ViVi!
-    //           <ArrowForwardIcon />
-    //         </span>
-    //       </ButtonLanding>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <video
-    //       autoPlay
-    //       loop
-    //       muted
-    //       className="d-block w-100"
-    //       loading="lazy"
-    //       preload="metadata"
-    //     >
-    //       <source src={colombia_4s} type="video/mp4" />
-    //     </video>
-    //     <Carousel.Caption>
-    //       <h3>Encuentra tu evento ideal</h3>
-    //       <p>
-    //         Descubre los festivales, carnavales, ferias y fiestas de Colombia
-    //         que invitan a la aventura de conocer este sorprendente país
-    //       </p>
-    //       <ButtonLanding onClick={() => navigate("/signin")}>
-    //         <span>
-    //           ¡Únete a ViVi!
-    //           <ArrowForwardIcon />
-    //         </span>
-    //       </ButtonLanding>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <video
-    //       autoPlay
-    //       loop
-    //       muted
-    //       className="d-block w-100"
-    //       loading="lazy"
-    //       preload="metadata"
-    //     >
-    //       <source src={colombia_5s} type="video/mp4" />
-    //     </video>
-    //     <Carousel.Caption>
-    //       <h3>Encuentra tu evento ideal</h3>
-    //       <p>
-    //         Descubre los festivales, carnavales, ferias y fiestas de Colombia
-    //         que invitan a la aventura de conocer este sorprendente país
-    //       </p>
-    //       <ButtonLanding onClick={() => navigate("/signin")}>
-    //         <span>
-    //           ¡Únete a ViVi!
-    //           <ArrowForwardIcon />
-    //         </span>
-    //       </ButtonLanding>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //  </Carousel>
+ 
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      as={CarouselVideos}
+      fade
+    >
+      <Carousel.Item>
+        <video
+          autoPlay
+          loop
+          muted
+          className="d-block w-100"
+          loading="lazy"
+          preload="metadata"
+        >
+          <source src={colombia_1s} type="video/mp4" />
+        </video>
+        <Carousel.Caption>
+          <h3>Encuentra tu evento ideal</h3>
+          <p>
+            Descubre los festivales, carnavales, ferias y fiestas de Colombia
+            que invitan a la aventura de conocer este sorprendente país
+          </p>
+          <ButtonLanding onClick={() => navigate("/signin")}>
+            <span>
+              ¡Únete a ViVi!
+              <ArrowForwardIcon />
+            </span>
+          </ButtonLanding>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <video
+          autoPlay
+          loop
+          muted
+          className="d-block w-100"
+          loading="lazy"
+          preload="metadata"
+        >
+          <source src={colombia_2s} type="video/mp4" />
+        </video>
+        <Carousel.Caption>
+          <h3>Encuentra tu evento ideal</h3>
+          <p>
+            Descubre los festivales, carnavales, ferias y fiestas de Colombia
+            que invitan a la aventura de conocer este sorprendente país
+          </p>
+          <ButtonLanding onClick={() => navigate("/signin")}>
+            <span>
+              ¡Únete a ViVi!
+              <ArrowForwardIcon />
+            </span>
+          </ButtonLanding>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <video
+          autoPlay
+          loop
+          muted
+          className="d-block w-100"
+          loading="lazy"
+          preload="metadata"
+        >
+          <source src={colombia_3s} type="video/mp4" />
+        </video>
+        <Carousel.Caption>
+          <h3>Encuentra tu evento ideal</h3>
+          <p>
+            Descubre los festivales, carnavales, ferias y fiestas de Colombia
+            que invitan a la aventura de conocer este sorprendente país
+          </p>
+          <ButtonLanding onClick={() => navigate("/signin")}>
+            <span>
+              ¡Únete a ViVi!
+              <ArrowForwardIcon />
+            </span>
+          </ButtonLanding>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <video
+          autoPlay
+          loop
+          muted
+          className="d-block w-100"
+          loading="lazy"
+          preload="metadata"
+        >
+          <source src={colombia_4s} type="video/mp4" />
+        </video>
+        <Carousel.Caption>
+          <h3>Encuentra tu evento ideal</h3>
+          <p>
+            Descubre los festivales, carnavales, ferias y fiestas de Colombia
+            que invitan a la aventura de conocer este sorprendente país
+          </p>
+          <ButtonLanding onClick={() => navigate("/signin")}>
+            <span>
+              ¡Únete a ViVi!
+              <ArrowForwardIcon />
+            </span>
+          </ButtonLanding>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <video
+          autoPlay
+          loop
+          muted
+          className="d-block w-100"
+          loading="lazy"
+          preload="metadata"
+        >
+          <source src={colombia_5s} type="video/mp4" />
+        </video>
+        <Carousel.Caption>
+          <h3>Encuentra tu evento ideal</h3>
+          <p>
+            Descubre los festivales, carnavales, ferias y fiestas de Colombia
+            que invitan a la aventura de conocer este sorprendente país
+          </p>
+          <ButtonLanding onClick={() => navigate("/signin")}>
+            <span>
+              ¡Únete a ViVi!
+              <ArrowForwardIcon />
+            </span>
+          </ButtonLanding>
+        </Carousel.Caption>
+      </Carousel.Item>
+     </Carousel>
   );
 };
